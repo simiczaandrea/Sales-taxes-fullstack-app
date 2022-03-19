d8bde66d575f330a3ec767e58f6701df585a2403
simicza.andrea <simicza.andrea@gmail.com>
2022-03-19 15:34:40 +0100

    project stucture created based on #2

---
+++ salestaxesbackend/src/main/java/com/salestaxes/salestaxesbackend/SalesTaxesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.salestaxes.salestaxesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesTaxesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesTaxesBackendApplication.class, args);
+    }
+
+}
